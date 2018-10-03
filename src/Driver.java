d251b0c321e14160d108d4ffcd0f2eee807b05a1
Buupu <sammyfattah@gmail.com>
2018-10-03 14:05:01 +0100

    Add files via upload

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,22 @@
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Driver {
     public static void main(String[] args) throws IOException {
         LSB lsb = new LSB();
-        BufferedImage img = lsb.getImageBuffer();
-        byte[] result = lsb.extractHiddenBytes(img);
+        BufferedImage img = lsb.getImageBuffer("img.bmp");
+        ArrayList<int[]> pixel = lsb.printRBG(img);
+        BufferedImage img2 = lsb.getImageBuffer("comp.bmp");
+        ArrayList<int[]> pixel2 = lsb.printRBG(img2);
+        String bCode = "";
+        for(int i=0; i<pixel.size(); i++) {
+        	if(pixel2.get(i)[0] != pixel.get(i)[0] && pixel2.get(i)[1] != pixel.get(i)[1] && pixel2.get(i)[2] != pixel.get(i)[2]) {
+        		System.out.println("Original: " + pixel2.get(i)[0] + " " + pixel2.get(i)[1] + " " + pixel2.get(i)[2]);
+        		System.out.println("Steg: " + pixel.get(i)[0] + " " + pixel.get(i)[1] + " " + pixel.get(i)[2]);
+        		bCode += String.valueOf((pixel.get(i)[0]-pixel2.get(i)[0])) + String.valueOf((pixel.get(i)[1]-pixel2.get(i)[1])) + String.valueOf((pixel.get(i)[2]-pixel2.get(i)[2]));
+        	}
+        }
+        System.out.println(bCode);
     }
 }
--- src/LSB.java
+++ src/LSB.java
@@ -7,18 +7,34 @@ import java.util.List;
 
 class LSB {
 
-    BufferedImage getImageBuffer() throws IOException {
-        BufferedImage img = null;
-        img = ImageIO.read(new File("img.bmp"));
-        return img;
-    }
+	BufferedImage getImageBuffer(String path) throws IOException {
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File(path));
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		return img;
+	}
 
-    byte[] extractHiddenBytes(BufferedImage img){
-        final int HIDDEN_FILE_SIZE = 83;
-        byte[] hiddenBytes = new byte[HIDDEN_FILE_SIZE];
+	byte[] extractHiddenBytes(BufferedImage img) {
+		final int HIDDEN_FILE_SIZE = 83;
+		byte[] hiddenBytes = new byte[HIDDEN_FILE_SIZE];
 
-        //todo implement
+		return hiddenBytes;
+	}
 
-        return hiddenBytes;
-    }
+	ArrayList<int[]> printRBG(BufferedImage img) {
+		ArrayList<int[]> pixel = new ArrayList<>();
+
+		for (int y = 0; y < img.getHeight(); y++) {
+			for (int x = 0; x < img.getWidth(); x++) {
+				pixel.add(img.getRaster().getPixel(x, y, new int[3]));
+//					System.out
+//							.println(pixel[0] + " - " + pixel[1] + " - " + pixel[2] + " - " + (img.getWidth() * y + x));
+				
+			}
+		}
+		return pixel;
+	}
 }
