e77ebbb4a0d18d8b6df455e26f51cfeff3dc98b1
Mark <mark.kerr.94+gitkraken@gmail.com>
2018-10-04 14:38:39 +0100

    initial solution

--- src/LSB.java
+++ src/LSB.java
@@ -6,7 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 class LSB {
-
+    public static void main(String[] args) throws IOException {
+        LSB lsb = new LSB();
+        List<Integer> result = lsb.decode(lsb.getImageBuffer("img.bmp"));
+        lsb.displayLSBS(result);
+    }
 	BufferedImage getImageBuffer(String path) throws IOException {
 		BufferedImage img = null;
 		try {
@@ -17,12 +21,26 @@ class LSB {
 		return img;
 	}
 
-	byte[] extractHiddenBytes(BufferedImage img) {
-		final int HIDDEN_FILE_SIZE = 83;
-		byte[] hiddenBytes = new byte[HIDDEN_FILE_SIZE];
-
-		return hiddenBytes;
+	List<Integer> decode(BufferedImage img) {
+        List<Integer> extracted_lsbs = new ArrayList<>();
+        for (int j = img.getWidth()-1; j >= 0; j--) {
+            Color current_pixel = new Color(img.getRGB(j, 639));
+            byte red_byte = (byte) current_pixel.getRed();
+            byte green_byte = (byte) current_pixel.getGreen();
+            byte blue_byte = (byte) current_pixel.getBlue();
+            int lsb_red = red_byte & 0x1;
+            int lsb_green = green_byte & 0x1;
+            int lsb_blue = blue_byte & 0x1;
+            extracted_lsbs.add(lsb_red);
+            extracted_lsbs.add(lsb_green);
+            extracted_lsbs.add(lsb_blue);
+        }
+		return extracted_lsbs;
 	}
+
+    void displayLSBS(List<Integer> integers) {
+        integers.forEach(System.out::print);
+    }
 
 	ArrayList<int[]> printRBG(BufferedImage img) {
 		ArrayList<int[]> pixel = new ArrayList<>();
